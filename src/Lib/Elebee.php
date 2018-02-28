949a77d6fca61630088f749337bf6c09701319c3
Holger Terhoeven <h.terhoeven@rto.de>
2018-02-28 13:05:17 +0100

    Use preview-hooks for enqueuing in preview.

--- src/Lib/Elebee.php
+++ src/Lib/Elebee.php
@@ -298,11 +298,11 @@ class Elebee {
         $this->loader->addAction( 'elementor/init', $elebeePublic, 'setupElementorCategories' );
         $this->loader->addAction( 'elementor/init', $elebeePublic, 'setupElementorExtensions' );
 
+        $this->loader->addAction( 'elementor/init', $elebeePublic, 'registerWidgets' );
+        $this->loader->addAction( 'elementor/init', $elebeePublic, 'registerExclusiveWidgets' );
+
         $this->loader->addAction( 'elementor/frontend/after_register_scripts', $elebeePublic, 'enqueueStyles' );
         $this->loader->addAction( 'elementor/frontend/after_register_scripts', $elebeePublic, 'enqueueScripts' );
-
-        $this->loader->addAction( 'elementor/widgets/widgets_registered', $elebeePublic, 'registerWidgets' );
-        $this->loader->addAction( 'elementor/widgets/widgets_registered', $elebeePublic, 'registerExclusiveWidgets' );
 
         $this->loader->addAction( 'elementor/widget/posts/skins_init', $elebeePublic, 'addWidgetPostsSkins' );
 
