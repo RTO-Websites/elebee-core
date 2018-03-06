a5e8faf35ef8b5b4b206648d50abe6d238c14651
Holger Terhoeven <h.terhoeven@rto.de>
2018-03-06 13:10:14 +0100

    Bump version.

--- src/Lib/Elebee.php
+++ src/Lib/Elebee.php
@@ -46,7 +46,7 @@ class Elebee {
      * @since 0.1.0
      * @var string The current version of the theme.
      */
-    const VERSION = '0.3.0';
+    const VERSION = '0.3.1';
 
     /**
      * The loader that's responsible for maintaining and registering all hooks that power
