ea979564870357f8ec0039bc8074e17820ab7c46
Holger Terhoeven <h.terhoeven@rto.de>
2018-10-01 16:43:07 +0200

    Remove obsolete setting registering.

--- src/Admin/ElebeeAdmin.php
+++ src/Admin/ElebeeAdmin.php
@@ -133,56 +133,7 @@ class ElebeeAdmin {
      */
     public function settingsApiInit() {
 
-//        add_settings_section(
-//            'elementor_rto_default_section',
-//            __('Settings', 'elebee'),
-//            [$this, 'sectionCallback'],
-//            'elementor_rto_settings'
-//        );
 
-        add_settings_field(
-            'is_exclusive',
-            __( 'Is Exclusive', 'elebee' ),
-            [ $this, 'renderSettingIsExclusive' ],
-            'elebee_settings'
-        );
-
-        register_setting( 'elebee_settings', 'is_exclusive' );
-
-        add_settings_field(
-            'jquery',
-            __( 'jQuery', 'elebee' ),
-            [ $this, 'renderSettingJQuery' ],
-            'elebee_settings'
-        );
-
-        register_setting( 'elebee_settings', 'jquery' );
-
-    }
-
-    public function renderSettingIsExclusive() {
-
-        echo '<input name="is_exclusive" id="is_exclusive" type="checkbox" value="1" ' . checked( 1, get_option( 'is_exclusive' ), false ) . '>';
-
-    }
-
-    public function renderSettingJQuery() {
-
-        $option = get_option( 'jquery', 'default' );
-
-        $choices = [
-            'default' => __( 'Default', 'elebee'),
-            'latest-cdn' => __( '3.3.1 (CDN)', 'elebee'),
-            'latest-local' => __( '3.3.1 (local)', 'elebee'),
-        ];
-
-        echo '<select name="jquery" id="jquery">';
-
-        foreach ( $choices as $value => $label ) {
-            echo '<option value="' . $value . '" ' . selected( $value, $option, false ) . '>' . $label . '</option>';
-        }
-
-        echo '</select>';
 
     }
 
