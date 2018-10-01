9cfb78f377ee7b6f640c6b91d5612bdb678cb6c1
Holger Terhoeven <h.terhoeven@rto.de>
2018-10-01 16:41:40 +0200

    Add concrete setting classes.

---
+++ src/Admin/Setting/IsExclusiv/SettingIsExclusiv.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * IsExclusiv.php
+ *
+ * @author  RTO GmbH <info@rto.de>
+ * @licence GPL-3.0
+ */
+
+namespace ElebeeCore\Admin\Setting\IsExclusiv;
+
+
+use ElebeeCore\Admin\Setting\SettingBase;
+use ElebeeCore\Lib\Util\Template;
+
+class SettingIsExclusiv extends SettingBase {
+
+    /**
+     * IsExclusiv constructor.
+     */
+    public function __construct() {
+
+        parent::__construct( 'is_exclusive', __( 'Is Exclusive', 'elebee' ), false );
+
+    }
+
+    public function render() {
+
+        $template = new Template( dirname( __DIR__ ) . '/partials/checkbox.php', [
+            'name' => $this->getName(),
+            'value' => 1,
+            'option' => $this->getOption(),
+        ] );
+        $template->render();
+
+    }
+
+}
