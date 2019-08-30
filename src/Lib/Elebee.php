8f3de7eb482da26da67c01a4efeefaca2a1427fb
Sascha Hennemann <s.hennemann@rto.de>
2019-08-30 16:04:16 +0200

    Change version

--- src/Lib/Elebee.php
+++ src/Lib/Elebee.php
@@ -60,7 +60,7 @@ class Elebee {
      * @since 0.1.0
      * @var string The current version of the theme.
      */
-    const VERSION = '0.7.2';
+    const VERSION = '0.8.0';
 
     /**
      * The loader that's responsible for maintaining and registering all hooks that power
